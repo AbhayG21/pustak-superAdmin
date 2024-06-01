48885a8d3b0503e0ef0facbf07bc305fb366ab9d
Abhay Gupta <abhay19021@gmail.com>
2024-06-01 21:18:24 +0530

    Initial Commit

---
+++ pustak-superAdmin/pustak_superAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  pustak_superAdminApp.swift
+//  pustak-superAdmin
+//
+//  Created by Abhay(IOS) on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct pustak_superAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
